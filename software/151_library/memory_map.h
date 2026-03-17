c5685a908cf24abe0dcecfcfd3bda7aa0e6150a2
Advay Ratan <advayratan@berkeley.edu>
2026-03-17 14:19:35 -0700

    baremetal C fixes

--- software/151_library/memory_map.h
+++ software/151_library/memory_map.h
@@ -4,8 +4,41 @@
     asm volatile ("csrw 0x51e,%[v]" :: [v]"r"(csr_val)); \
 }
 
-#define COUNTER_RST (*((volatile uint32_t*) 0x80000018))
-#define CYCLE_COUNTER (*((volatile uint32_t*)0x80000010))
-#define INSTRUCTION_COUNTER (*((volatile uint32_t*)0x80000014))
-#define BRANCH_INSTRUCTION_COUNTER (*((volatile uint32_t*)0x8000001c))
-#define BRANCH_PREDICTION_CORRECT_COUNTER (*((volatile uint32_t*)0x80000020))
+
+// Reset mcycle and minstret (low and high words)
+static inline void reset_counters(void) {
+    asm volatile("csrwi 0x802, 0\n\t"  // minstret (low) = 0
+                 "csrwi 0xB82, 0"      // minstreth (high) = 0
+                 ::: "memory");
+    asm volatile("csrwi 0x800, 0\n\t"  // mcycle (low) = 0
+                 "csrwi 0xB80, 0"      // mcycleh (high) = 0
+                 ::: "memory");
+}
+
+// Read instruction-retired counter (instret/h)
+static inline uint32_t read_instret_counter(void) {
+    uint32_t value;
+    asm volatile("csrr %0, 0x802" : "=r"(value));
+    return value;
+}
+
+// Read high word of instruction-retired counter (instreth)
+static inline uint32_t read_instret_counter_high(void) {
+    uint32_t value;
+    asm volatile("csrr %0, 0xB82" : "=r"(value));
+    return value;
+}
+
+// Read cycle counter (cycle)
+static inline uint32_t read_cycle_counter(void) {
+    uint32_t value;
+    asm volatile("csrr %0, 0x800" : "=r"(value));
+    return value;
+}
+
+// Read high word of cycle counter (cycleh)
+static inline uint32_t read_cycle_counter_high(void) {
+    uint32_t value;
+    asm volatile("csrr %0, 0xB80" : "=r"(value));
+    return value;
+}
