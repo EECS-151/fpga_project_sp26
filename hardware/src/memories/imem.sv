46c73a6a74870182fe68a52506d6698f67265642
Advay Ratan <advayratan@berkeley.edu>
2026-03-19 17:41:09 -0700

    increased imem and dmem size in preparation for checkpoint 3

--- hardware/src/memories/imem.sv
+++ hardware/src/memories/imem.sv
@@ -2,17 +2,17 @@ module imem (
   input logic clk,
   input logic ena,
   input logic [3:0] wea,
-  input logic [13:0] addra,
+  input logic [14:0] addra,
   input logic [31:0] dina,
-  input logic [13:0] addrb,
+  input logic [14:0] addrb,
   output logic [31:0] doutb
 );
-  parameter DEPTH = 16384;
+  parameter DEPTH = 32768;
 
   // See page 133 of the Vivado Synthesis Guide for the template
   // https://www.xilinx.com/support/documentation/sw_manuals/xilinx2016_4/ug901-vivado-synthesis.pdf
 
-  logic [31:0] mem [16384-1:0];
+  logic [31:0] mem [DEPTH-1:0];
   integer i;
   always @(posedge clk) begin
     if (ena) begin
