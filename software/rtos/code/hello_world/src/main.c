e50c8a831aaadbc2b77b4f68bbc83a51cbc3274f
Advay Ratan <advayratan@berkeley.edu>
2026-04-09 16:53:51 -0700

    coremark setup

--- software/rtos/code/hello_world/src/main.c
+++ software/rtos/code/hello_world/src/main.c
@@ -7,6 +7,18 @@
 #include <zephyr.h>
 #include <sys/printk.h>
 
+#include <shell/shell.h>
+
+extern int coremark_main(int argc, char *argv[]);
+
+static int cmd_run_coremark(const struct shell *shell, size_t argc, char **argv)
+{
+    shell_print(shell, "Starting CoreMark");
+    return coremark_main(argc, argv);
+}
+
+SHELL_CMD_REGISTER(coremark, NULL, "Run CoreMark benchmark", cmd_run_coremark);
+
 void main(void)
 {
 	printk("Hello World! %s\n", CONFIG_BOARD);
